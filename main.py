6527e766030b5b95df64e15fa6f79d4d89de188c
nextebiz <nextebiz.com@gmail.com>
2024-06-18 19:15:48 +0500

    new route counter

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import uvicorn
 
 app = fastapi.FastAPI()
 
+count = 0
+
+def get_count():
+    global count
+    count = count+1
+    return count
+
 @app.get('/')
 def home():
     return "home"
+
+@app.get('/counter')
+def counter():
+    return "counting : " + str(get_count())
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=2200)
